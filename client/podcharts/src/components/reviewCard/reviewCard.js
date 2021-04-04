529b7b7f6d5eee42ecc86d6f0076cebcc75cc658
KashifIV <kashif.h77@gmail.com>
2021-04-03 20:43:24 -0400

    wrote basic review post

---
+++ client/podcharts/src/components/reviewCard/reviewCard.js
@@ -0,0 +1,25 @@
+import React from 'react'; 
+import {Card, Media, Content, Heading} from 'react-bulma-components'; 
+
+import './reviewCard.css'; 
+
+function ReviewCard(props){
+
+  const {review} = props; 
+
+  return <div className="review-card">
+    <Card>
+      <Card.Content>
+        <Media.Item>
+          <Heading size={4}>{review.title}</Heading>
+          <Heading subtitle size={6}>{review.author}</Heading>
+        </Media.Item>
+        <Content className="content">
+          {review.content}
+        </Content>
+      </Card.Content>
+    </Card>
+  </div>
+}
+
+export default ReviewCard; 
--- client/podcharts/src/pages/podcast/podcast.js
+++ client/podcharts/src/pages/podcast/podcast.js
@@ -3,9 +3,13 @@ import Loading from '../../components/loading/loading';
 import BannerImage from '../../components/bannerImage/bannerImage'; 
 import EpisodeTile from '../../components/episodeTile/episodeTile'; 
 import TopicHeader from '../../components/topicHeader/topicHeader'; 
+import ReviewCard from '../../components/reviewCard/reviewCard'; 
+
+import './podcast.css'; 
 
 // temporary data 
 import podcastData from './tempPodcastData.json';
+import reviewData from './tempReviewData.json'; 
 class Podcast extends Component{
   constructor(props){
     super(); 
@@ -16,7 +20,8 @@ class Podcast extends Component{
   componentDidMount(){
     console.log(podcastData); 
     this.setState({
-      data: podcastData
+      data: podcastData, 
+      reviews: reviewData 
     }); 
   }
 
@@ -27,11 +32,29 @@ class Podcast extends Component{
     else{
       return <div className="podcast-page">
         <BannerImage image={this.state.data.episodes.items[0]['images'][0]['url']} title={this.state.data.name} description={this.state.data.description}/>
+        <TopicHeader text="Latest Episodes"/>
         <div className="episodes">
-          <TopicHeader text="Latest Episodes"/>
-          {
-            this.state.data.episodes.items.map((value) => <EpisodeTile episode={value}/>)
-          }
+          <ul>
+            {
+              this.state.data.episodes.items.map((value) => <li key={value.uri}>
+                  <EpisodeTile episode={value}/>
+                </li>)
+            }
+          </ul>
+        </div>
+        <div className="clear"></div>
+
+        <TopicHeader text="Reviews"/>
+        <div className="reviews">
+          <ul>
+            {
+              this.state.reviews.items.map((item) => 
+                <li>
+                  <ReviewCard review={item}/>
+                </li>
+              )
+            }
+          </ul>
         </div>
       </div>
     }
