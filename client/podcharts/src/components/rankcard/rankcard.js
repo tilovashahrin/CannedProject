daaa3c1e774a539c6e7808f1366eed1cf5bab25d
Sunny Cao <Yiqing.Cao@uoit.net>
2021-04-15 23:49:08 -0400

    populate ranked podcasts and added onclick handler to redirect to podcast page

--- client/podcharts/src/components/rankcard/rankcard.js
+++ client/podcharts/src/components/rankcard/rankcard.js
@@ -1,24 +1,37 @@
 import React from "react";
 import 'react-bulma-components/dist/react-bulma-components.min.css';
-import { Card, Media, Content, Heading, Image, Container } from 'react-bulma-components';
+import { Card, Media, Content, Heading, Image, Section, Container } from 'react-bulma-components';
 import "./rankcard.css"
-// import {Card, Media, Content, Heading} from 'react-bulma-components'; 
+import {useHistory} from 'react-router-dom'; 
 
 function Rankcard(props) {
     // const { title, image, description, rating, creator } = props
-    const { image, title, description, creator, rating, ranking } = props
-    console.log(image)
-    return (
-        // <Container>
+    const { value, rank } = props
+    const history = useHistory(); 
 
+    const image = value.episodes.items[0]['images'][0]['url'];
+    const title = value.name;
+    const description = value.description;
+    const creator = value.publisher;
+    const rating = 8; // value.rating
+    const ranking = rank;
+    const id = value.id;
+
+    // console.log(image)
+
+    const onClickItem = () => {
+        history.push({pathname: '/podcast', state:{podcastID: id}}); 
+      }
+
+    return (
         <Card id="cardItem" className="columns">
             {/* <Card.Image src={image} size="2by3" className="column is-2"/> */}
             <Card.Content className="column is-1" id="rank">
-                <div  id="rankBox" className="content container has-text-centered">
+                <div id="rankBox" className="content has-text-centered">
                     <h1>Rank #{ranking} </h1>
                 </div>
             </Card.Content>
-            <Card.Content className="column is-8">
+            <Card.Content className="column is-8" onClick={onClickItem}>
                 <Media>
                     <Media.Item renderAs="figure" position="left">
                         <Image src={image} size={64} />
@@ -28,7 +41,7 @@ function Rankcard(props) {
                         <Heading size={4}>{title}</Heading>
                         <Heading subtitle size={6}>By {creator}</Heading>
                     </Media.Item>
-                    
+
                     <Media.Item position="right">
                         {/* <Image src={image} size={64} /> */}
                         <div className="content">
@@ -45,29 +58,6 @@ function Rankcard(props) {
                 </Content>
             </Card.Content>
         </Card>
-        // </Container>
-
-        // <div className="card">
-        //     <div className="card-image">
-        //         <figure className="image is-4by3">
-        //             <img scr={image} height="100" width="300"></img>
-        //         </figure>
-        //     </div>
-        //     <div className="card-content">
-
-        //         <div className="media">
-        //                 <div className="media-content">
-        //                     <p className="title is-4">{title}</p>
-        //                     <p className="subtitle is-6">sub title</p>
-        //                 </div>
-        //             </div>
-
-        //             <div className="content">
-        //                 This is a show case of Rankcard
-        //                 {description}
-        //             </div>
-        //         </div>
-        //     </div>
     );
 }
 
--- client/podcharts/src/pages/home/home.js
+++ client/podcharts/src/pages/home/home.js
@@ -22,11 +22,14 @@ class Home extends Component {
     }
 
     render() {
+
         if (this.state.data == null) {
             return <Loading />
         }
         else {
-            return <div className="home-page section">
+            // need to take a rank-sorted list of podcast 
+            let rank = 0;
+            return <div className="home-page section has-text-left p-0 m-0">
                 <section className="hero" id="ranktitle">
                     <div className="hero-body">
                         <p className="title">
@@ -37,8 +40,23 @@ class Home extends Component {
                         </p>
                     </div>
                 </section>
-                <div className="container section">
-                    <Rankcard image={this.state.data.episodes.items[0]['images'][0]['url']} title={this.state.data.name} description={this.state.data.description} creator={this.state.data.publisher} rating={8} ranking={1} />
+                <div className="container section p-0 m-0">
+                    <ul>
+                        {
+                            this.state.data.map(function (value) {
+                                console.log(value)
+                                rank += 1;
+                                return (<li key={value.uri}>
+                                    <div class="section">
+                                        {/* <Rankcard image={value.episodes.items[0]['images'][0]['url']} title={value.name} description={value.description} creator={value.publisher} rating={8} ranking={rank} /> */}
+                                        <Rankcard value={value} rank={rank} />
+                                    </div>
+                                </li>
+                                )
+                            })
+                        }
+                    </ul>
+                    {/* <Rankcard image={this.state.data.episodes.items[0]['images'][0]['url']} title={this.state.data.name} description={this.state.data.description} creator={this.state.data.publisher} rating={8} ranking={1} /> */}
                 </div>
             </div>
         }
