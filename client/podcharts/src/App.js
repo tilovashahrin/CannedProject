08df3386933dc63043014a761beb7d53066a5a8c
Sunny Cao <Yiqing.Cao@uoit.net>
2021-04-15 20:14:53 -0400

    did small changes

--- client/podcharts/src/App.js
+++ client/podcharts/src/App.js
@@ -1,31 +1,31 @@
-import {BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import NavBar from './components/navbar/navbar';
 
-import Home from './pages/home/home'; 
-import Account from './pages/account/account'; 
-import Podcast from './pages/podcast/podcast'; 
+import Home from './pages/home/home';
+import Account from './pages/account/account';
+import Podcast from './pages/podcast/podcast';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
 
-    <Router>
-      <NavBar/>
+      <Router>
+        <NavBar />
 
-      <Switch>
-      <Route path="/podcast">
-          <Podcast/>
-        </Route>
-      <Route path="/account">
-          <Account/>
-        </Route>
-        <Route path="/">
-          <Home/>
-        </Route>
+        <Switch>
+          <Route path="/podcast">
+            <Podcast />
+          </Route>
+          <Route path="/account">
+            <Account />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
 
-      </Switch>
-    </Router>
+        </Switch>
+      </Router>
 
     </div>
   );
