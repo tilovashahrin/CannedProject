862492c723d0621d68a8980e09b02ecda1ce8fe9
Sunny Cao <Yiqing.Cao@uoit.net>
2021-04-19 17:42:46 -0400

    now it has the uri, I need to add ip address for one to access the db so it should be fine

--- server/server.js
+++ server/server.js
@@ -4,14 +4,24 @@ let app = express();
 // const user_model = require('./model/user_model.js') // import from local machine
 
 const { MongoClient } = require('mongodb');
-const secrets = require('secrets')
+const uri = 'mongodb+srv://sunny:NpeyKCnXQtW3S1wU@cluster0.1hv4s.mongodb.net/cannedpods?retryWrites=true&w=majority'
+const client = new MongoClient(uri);
 
-async function main() {
-  const client = new MongoClient(uri);
+async function test(client) {
   
   try {
+
     await client.connect();
-    await listDatabases(client);
+    // await listDatabases(client);
+    const database = client.db('CannedPods');
+    const reviews = database.collection('review');
+
+    const query = {id: '1'}
+
+    const review = await reviews.findOne(query);
+
+    console.log(review)
+
   } catch (e) {
     console.error(e)
   } finally {
@@ -27,7 +37,8 @@ async function listDatabases(client) {
   databasesList.databases.forEach(db => console.log(` - ${db.name}`))
 }
 
-main().catch(console.error);
+// main().catch(console.error);
+test(client).catch(console.error);
 
 // async function test(client) {
 // }
