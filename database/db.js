43cfe97fa71034354f9a6e24e1a7d83abbaa1ebb
PulkitMadan <pulkit.madan@ontariotechu.net>
2021-04-19 18:24:50 -0400

    added collections and schema for podcasts and reviews

--- database/db.js
+++ database/db.js
@@ -27,4 +27,77 @@ let accountsSchema = new Schema({
     collection: 'accounts'
 }); 
 
+
+let reviewSchema = new Schema({
+    "RatingPercentage": [Number], 
+    "items": [{
+        "author": String, 
+        "rating": Number, 
+        "title": String, 
+        "content": String, 
+        "id": String,
+    }]
+}, {
+    collection: 'reviews'
+});
+
+let podcastSchema = new Schema(
+    {
+        "available_markets" : [String],
+        "description" : String,
+        "episodes" : {
+          "href" : String,
+          "items" : [{
+            "audio_preview_url" : String,
+            "description" : String, 
+            "duration_ms" : Number,
+            "external_urls" : {
+              "spotify" : String
+            },
+            "href" : String,
+            "id" : String,
+            "images" : [ {
+              "height" : Number,
+              "url" : String,
+              "width" : Number
+            }],
+            "is_externally_hosted" : Boolean,
+            "language" : String,
+            "name" : String,
+            "release_date" : Date,
+            "release_date_precision" : String,
+            "type" :String,
+            "uri" :String
+          }],
+          "limit" : Number,
+          "next" : String,
+          "offset" : Number,
+          "previous" : {type = String, default: null},
+          "total" : Number
+        },
+        "explicit" : Boolean,
+        "external_urls" : {
+          "spotify" : String
+        },
+        "href" : String,
+        "id" : String,
+        "images" : [ {
+          "height" : Number,
+          "url" : String,
+          "width" : Number
+        }],
+        "is_externally_hosted" :Boolean,
+        "languages" : [String],
+        "media_type" : String,
+        "name" :String,
+        "publisher" : String,
+        "type" : String,
+        "uri" : String
+    }
+    , {
+    collection = 'podcasts'
+});
+
 module.exports.account = mongoose.model('account', accountsSchema);
+module.exports.review = mongoose.model('review', reviewSchema); 
+module.exports.podcast = mongoose.model('podcast', podcastSchema);
